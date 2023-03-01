fa6e7811a5b9d8709915725f2a49031fb5faecd4
kpw6 <kpw6@njit.edu>
2023-02-28 21:41:19 -0500

    Added camera movement for world

---
+++ Assets/Scripts/Camera-Movement/CameraMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    Vector3 cPosition;
+
+    public float cameraSpeed = 50f;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        cPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            cPosition.x -= cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            cPosition.x += cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            cPosition.z += cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            cPosition.z -= cameraSpeed * Time.deltaTime;
+        }
+        
+        this.transform.position = cPosition;
+    }
+}
