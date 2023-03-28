04fb3dde69fe92e383cb9eab88a877769365d2a8
kpw6 <kpw6@njit.edu>
2023-03-27 20:46:17 -0400

    Added the starts to movement

---
+++ Assets/Scripts/Player/Character.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    HexCell currentCell; //The current cell that the ship is over
+
+    public void updatePosition(HexCell cell) {
+        currentCell = cell;
+        //Debug.Log(currentCell.Position.ToString());
+        transform.position = new Vector3(currentCell.Position.x, currentCell.WaterSurfaceY, currentCell.Position.z);
+    }
+
+}
