b7715f57c86aa9f1d852d224f7bfb636a7a92590
kpw6 <kpw6@njit.edu>
2023-04-16 22:03:57 -0400

    Enhanced Movement and Added Card Data to cards

--- Assets/Scripts/Player/Cards/CardList.cs
+++ Assets/Scripts/Player/Cards/CardList.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class CardList : MonoBehaviour
 {
+
+    public TextAsset json;
+
+    [System.Serializable]
+    public class createUnitCardsList
+    {
+        public Card[] cards;
+    }
+
+    public createUnitCardsList UnitCards = new createUnitCardsList(); 
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnitCards = JsonUtility.FromJson<createUnitCardsList>(json.text);
+        Debug.Log("successfull");
     }
 
     // Update is called once per frame
