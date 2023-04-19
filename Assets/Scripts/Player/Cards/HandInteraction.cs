0d9c14243d0a0506dffc2bfba5313d3b62c980f3
cjohnjoyce <cjohnjoyce@gmail.com>
2023-04-19 18:46:46 -0400

    working hands and more ui stuff

--- Assets/Scripts/Player/Cards/HandInteraction.cs
+++ Assets/Scripts/Player/Cards/HandInteraction.cs
@@ -28,30 +28,39 @@ public class HandInteraction : MonoBehaviour
             //instantiate a card hand prefab as a child of playerhand when given card name
             //position changed relative to how many cards are in hand
             //currently instantiates just guppy goon but should be dependant on String card input
-            if(card is "Guppy Goon")
-                Instantiate(guppyGoon, Player1Hand);
+
+            
+            if (card is "Guppy Goon")
+                Instantiate(guppyGoon, new Vector3(Player1Hand.position.x-330+(150*Player1HandSize), Player1Hand.position.y-190, Player1Hand.position.z), Quaternion.identity, Player1Hand);
             if (card is "Stabby Crab")
-                Instantiate(stabbyCrab, Player1Hand);
+                Instantiate(stabbyCrab, new Vector3(Player1Hand.position.x - 330 + (150 * Player1HandSize), Player1Hand.position.y - 190, Player1Hand.position.z), Quaternion.identity, Player1Hand);
             if (card is "Hired Mussel")
-                Instantiate(hiredMussel, Player1Hand);
+                Instantiate(hiredMussel, new Vector3(Player1Hand.position.x - 330 + (150 * Player1HandSize), Player1Hand.position.y - 190, Player1Hand.position.z), Quaternion.identity, Player1Hand);
         }
         if (player.name is "Player 2") {
             Player2HandSize++;
             Debug.Log(card + " should be given to " + player.name);
             if (card is "Guppy Goon")
-                Instantiate(guppyGoon, Player2Hand);
+                Instantiate(guppyGoon, new Vector3(Player2Hand.position.x - 330 + (150 * Player2HandSize), Player2Hand.position.y - 190, Player2Hand.position.z), Quaternion.identity, Player2Hand);
             if (card is "Stabby Crab")
-                Instantiate(stabbyCrab, Player2Hand);
+                Instantiate(stabbyCrab, new Vector3(Player2Hand.position.x - 330 + (150 * Player2HandSize), Player2Hand.position.y - 190, Player2Hand.position.z), Quaternion.identity, Player2Hand);
             if (card is "Hired Mussel")
-                Instantiate(hiredMussel, Player2Hand);
-
+                Instantiate(hiredMussel, new Vector3(Player2Hand.position.x - 330 + (150 * Player2HandSize), Player2Hand.position.y - 190, Player2Hand.position.z), Quaternion.identity, Player2Hand);
         }
-        
+
     }
 
     public void HandleSelectCard()
     {
+        //when the card's button is clicked allow the player to click a hex and instantiate the respective unit
+        //also instantiates unit card to the first open army field slot
+    }
 
+    public void HandleDiscardCard()
+    {
+        //card is removed from play and added to the list of discarded cards
+        //decrement player hand size
+        //rearrange card positions
     }
 
     // Start is called before the first frame update
