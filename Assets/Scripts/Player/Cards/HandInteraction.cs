4dd163d200acc6b00ab05c6c381441fd8abde3df
cjohnjoyce <cjohnjoyce@gmail.com>
2023-04-19 17:14:24 -0400

    player hand differentiation and improved recruit phase

--- Assets/Scripts/Player/Cards/HandInteraction.cs
+++ Assets/Scripts/Player/Cards/HandInteraction.cs
@@ -11,6 +11,8 @@ public class HandInteraction : MonoBehaviour
     public Transform Player1Hand;
     public Transform Player2Hand;
     public GameObject guppyGoon;
+    public GameObject stabbyCrab;
+    public GameObject hiredMussel;
 
 
     //chris is responsible for the below code that adds card to player hands
@@ -26,12 +28,23 @@ public class HandInteraction : MonoBehaviour
             //instantiate a card hand prefab as a child of playerhand when given card name
             //position changed relative to how many cards are in hand
             //currently instantiates just guppy goon but should be dependant on String card input
-            Instantiate(guppyGoon, Player1Hand);
+            if(card is "Guppy Goon")
+                Instantiate(guppyGoon, Player1Hand);
+            if (card is "Stabby Crab")
+                Instantiate(stabbyCrab, Player1Hand);
+            if (card is "Hired Mussel")
+                Instantiate(hiredMussel, Player1Hand);
         }
         if (player.name is "Player 2") {
             Player2HandSize++;
             Debug.Log(card + " should be given to " + player.name);
-            
+            if (card is "Guppy Goon")
+                Instantiate(guppyGoon, Player2Hand);
+            if (card is "Stabby Crab")
+                Instantiate(stabbyCrab, Player2Hand);
+            if (card is "Hired Mussel")
+                Instantiate(hiredMussel, Player2Hand);
+
         }
         
     }
